c19d73064a9ffd0f0ad39acd4dc42f5041c05c05
Malvina <malvink@bk.ru>
2022-12-06 23:30:20 +0300

    add task 013

--- HW_Seminar2_Tasks10,13,15/Program.cs
+++ HW_Seminar2_Tasks10,13,15/Program.cs
@@ -3,17 +3,41 @@
 // 782 -> 8
 // 918 -> 1
 
+//Console.Write("Введите число: ");
+//int number = int.Parse(Console.ReadLine());
+
+//if (number >99 &&  number < 1000)
+//{
+ //   int secondDigit= (number/10)%10;
+ //   Console.WriteLine($"Вторая цифра в числе = {secondDigit}");
+//}
+//else
+//Console.WriteLine("Число не трехзначное");
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 
-if (number >99 &&  number < 1000)
-{
-    int secondDigit= (number/10)%10;
-    Console.WriteLine($"Вторая цифра в числе = {secondDigit}");
+if (number> 99 && number < 1000)
+{ 
+     int thirdDigit = number % 10;
+    Console.WriteLine($"Третья цифра в числе = {thirdDigit}");
 }
-else
-Console.WriteLine("Число не трехзначное");
 
-
+if (number>1000)
+{
+  int thirdDigit = (number /=100)%10;
+  Console.WriteLine($"Третья цифра в числе = {thirdDigit}");
+}
+  else
+     
+      Console.WriteLine($"Третьей цифры в числе нет");
+    
 
 
