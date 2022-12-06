d0c894b23950e921c5580f0a64dce9ad0c51d5fe
Malvina <malvink@bk.ru>
2022-12-06 23:25:20 +0300

    add task 010

---
+++ HW_Seminar2_Tasks10,13,15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number >99 &&  number < 1000)
+{
+    int secondDigit= (number/10)%10;
+    Console.WriteLine($"Вторая цифра в числе = {secondDigit}");
+}
+else
+Console.WriteLine("Число не трехзначное");
+
+
+
+
